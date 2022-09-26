098f334a8678d39584e7c22cd13beb2156c3d0b2
Sebastian <sebastian@szvetecz.at>
2022-09-26 10:47:20 +0200

    Added sample

---
+++ BindableLayoutBindingListBugSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BindableLayoutBindingListBugSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BindableLayoutBindingListBugSample/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+namespace BindableLayoutBindingListBugSample;
+
+public partial class MainPage : ContentPage
+{
+	private int _items = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+
+	public string FrameworkVersion { get; } = Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName ?? string.Empty;
+
+	public BindingList<int> LeftItems { get; } = new();
+
+	public ObservableCollection<int> RightItems { get; } = new();
+
+	private void AddItemButton_Clicked(object sender, EventArgs e)
+	{
+		LeftItems.Add(_items);
+		RightItems.Add(_items);
+        _items++;
+    }
+}
+
